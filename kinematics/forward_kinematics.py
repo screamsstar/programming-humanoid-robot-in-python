daaca7a4da5475dd37994c24c9afb2bd96679fae
Vito Mengers <v.mengers@campus.tu-berlin.de>
2018-06-01 17:13:40 +0200

    forward kinematics done, no clue how to test them right now

--- kinematics/forward_kinematics.py
+++ kinematics/forward_kinematics.py
@@ -20,7 +20,7 @@ import os
 import sys
 sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'joint_control'))
 
-from numpy.matlib import matrix, identity
+from numpy.matlib import matrix, identity, dot
 import math
 
 from angle_interpolation import AngleInterpolationAgent
@@ -36,12 +36,49 @@ class ForwardKinematicsAgent(AngleInterpolationAgent):
         self.transforms = {n: identity(4) for n in self.joint_names}
 
         # chains defines the name of chain and joints of the chain
-        self.chains = {'Head': ['HeadYaw', 'HeadPitch']
+        self.chains = {'Head': ['HeadYaw', 'HeadPitch'],
                        'LArm': ['LShoulderPitch', 'LShoulderRoll', 'LElbowYaw', 'LElbowRoll'],
                        'RArm': ['RShoulderPitch', 'RShoulderRoll', 'RElbowYaw', 'RElbowRoll'],
                        'LLeg': ['LHipYawPitch', 'LHipRoll', 'LHipPitch', 'LKneePitch', 'LAnklePitch', 'LAnkleRoll'],
                        'RLeg': ['RHipYawPitch', 'RHipRoll', 'RHipPitch', 'RKneePitch', 'RAnklePitch', 'RAnkleRoll']
                        }
+        self.yaw_rotators=['HeadYaw','LElbowRoll', 'RElbowRoll', 'LShoulderRoll', 'RShoulderRoll'] # this is so weird
+        self.roll_rotators=['LHipRoll', 'LAnkleRoll', 'RHipRoll', 'RAnkleRoll', 'LElbowYaw', 'RElbowYaw']
+        self.pitch_rotators = ['HeadPitch', 'LShoulderPitch', 'RShoulderPitch','LHipPitch', 'LKneePitch', 'LAnklePitch', 'RHipPitch', 'RKneePitch', 'RAnklePitch']
+
+        # great task... this took way to long
+        self.distances = {# Head
+                        'HeadYaw': [0, 0, 126.5],
+                        'HeadPitch': [0, 0, 0],
+
+                        # Left arm
+                        'LShoulderPitch': [0, 98, 100],
+                        'LShoulderRoll': [0, 0, 0],
+                        'LElbowYaw': [105, 15, 0],
+                        'LElbowRoll': [0, 0, 0],
+
+                        # Right arm
+                        'RShoulderPitch': [0, -98, 100],
+                        'RShoulderRoll': [0, 0, 0],
+                        'RElbowYaw': [105, -15, 0],
+                        'RElbowRoll': [0, 0, 0],
+
+                        # Left leg
+                        'LHipYawPitch': [0, 50, -85],
+                        'LHipRoll': [0, 0, 0],
+                        'LHipPitch': [0, 0, 0],
+                        'LKneePitch': [0, 0, -100],
+                        'LAnklePitch': [0, 0, -102.9],
+                        'LAnkleRoll': [0, 0, 0],
+
+                        # Right leg
+                        'RHipYawPitch': [0, -50, -85],
+                        'RHipRoll': [0, 0, 0],
+                        'RHipPitch': [0, 0, 0],
+                        'RKneePitch': [0, 0, -100],
+                        'RAnklePitch': [0, 0, -102.9],
+                        'RAnkleRoll': [0, 0, 0],
+                        }
 
     def think(self, perception):
         self.forward_kinematics(perception.joint)
@@ -57,22 +94,35 @@ class ForwardKinematicsAgent(AngleInterpolationAgent):
         '''
         T = identity(4)
         # YOUR CODE HERE
-        axis_unit_vector = [0,0,0]
-        # TODO determine axis
+        # determine right rotation_matrix values
+        normal_value_for_2 = math.sqrt(0.5)
+
+        if self.yaw_rotators.__contains__(joint_name):
+            ux, uy, uz = 0, 0, 1
+        elif self.roll_rotators.__contains__(joint_name):
+            ux, uy, uz = 1, 0, 0
+        elif self.pitch_rotators.__contains__(joint_name):
+            ux, uy, uz = 0, 1, 0
+        elif joint_name == 'LHipYawPitch':
+            ux, uy, uz = 0, normal_value_for_2, -normal_value_for_2
+        elif joint_name == 'RHipYawPitch':
+            ux, uy, uz = 0, -normal_value_for_2, -normal_value_for_2
+        else:
+            print "weird jointname"
+            ux, uy, uz = 0, 0, 0
 
         cos = math.cos(joint_angle)
         sin = math.sin(joint_angle)
-        normalizer = math.sqrt(math.fsum([x**2 for x in axis_unit_vector]))
-        ux = axis_unit_vector[0] / normalizer
-        uy = axis_unit_vector[1] / normalizer
-        uz = axis_unit_vector[2] / normalizer
         # rotation matrix in 3d around any normalized axis
         rotation_matrix = matrix([[cos+ux**2*(1-cos), ux*uy*(1-cos)-uz*sin, ux*uz*(1-cos)+uy*sin],
                                   [uy*ux*(1-cos)+uz*sin, cos+uy**2*(1-cos), uy*uz*(1-cos)-ux*sin],
                                   [uz*ux*(1-cos)-uy*sin, uz*uy*(1-cos)+ux*sin, cos+uz**2*(1-cos)]])
 
         T[0:3, 0:3] = rotation_matrix
-        # TODO set T[0:2, 3] right according to distances
+        # set T[0:2, 3] right according to distances
+        distance_lengths = self.distances[joint_name]
+        for i in range(3):
+            T[i, 3] = distance_lengths[i]
 
         return T
 
@@ -87,8 +137,11 @@ class ForwardKinematicsAgent(AngleInterpolationAgent):
                 angle = joints[joint]
                 Tl = self.local_trans(joint, angle)
                 # YOUR CODE HERE
-
+                T = dot(T, Tl)
+                print(joint)
+                print(T)
                 self.transforms[joint] = T
+
 
 if __name__ == '__main__':
     agent = ForwardKinematicsAgent()
